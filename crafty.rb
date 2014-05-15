cc2192ca6f6c35c788bd8937914d9d362b3a3803
Larry Gilbert <larry@l2g.to>
2014-05-14 21:56:31 -0700

    New formula for Crafty 23.8 (chess engine)

---
+++ crafty.rb
@@ -0,0 +1,23 @@
+# TODO:
+# * Install the opening-moves book
+# * Install help
+# * Determine number of cores and configure appropriately
+
+require 'formula'
+
+class Crafty < Formula
+  # Look for newer versions at http://www.cis.uab.edu/hyatt/crafty/source/?C=M;O=D
+  # because the home page may not be up-to-date
+  homepage 'http://www.craftychess.com/'
+  url 'http://www.cis.uab.edu/hyatt/crafty/source/crafty-23.8.zip'
+  sha1 '662ae1f97727eacfae0cff0a8e6449a45178648d'
+
+  def install
+    system 'make', 'darwin'
+    bin.install 'crafty'
+  end
+
+  test do
+    system 'crafty end'
+  end
+end
